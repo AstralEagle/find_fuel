f124bb80fec525dbf6bf5f873c887c6cf7839228
AstralEagle <arthur.dias@live.fr>
2022-10-20 18:00:15 +0200

    Ajout du header avec la localision

--- src/Componante/Index.jsx
+++ src/Componante/Index.jsx
@@ -1,18 +1,38 @@
-import '../Style/style.css';
+import { useState, useRef } from "react";
+import GooglePlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-google-places-autocomplete";
+
+import "../Style/style.css";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function () {
-    return(
-        <div id="mainDiv">
-            <header>
-                <h1>Find fuel</h1>
-                <form>
-                    <input type="text" name="search"/>
-                    <input type="submit" value="search"/>
-                </form>
-            </header>
+  const [adress, setAdress] = useState("");
 
+  //On actualise la localisation
+  const selectAdress = (adress) => {
+    geocodeByAddress(adress.label)
+      .then((results) => getLatLng(results[0]))
+      .then(({ lat, lng }) => {
+        console.log("Successfully got latitude and longitude", { lat, lng });
+      })
+      .catch((error) => console.error(error));
+  }
 
-        </div>
-    )
+  return (
+    <div id="mainDiv">
+      <header>
+        <h1 >Find fuel</h1>
+        <GooglePlacesAutocomplete
+          apiKey={"AIzaSyCAjZncD6io9xEKzazHtU-WmH-_UIkTOn0"}
+          apiOptions={{ language: "fr", region: "fr" }}
+          selectProps={{
+            adress,
+            onChange: selectAdress,
+          }}
+        />
+      </header>
+    </div>
+  );
 }
